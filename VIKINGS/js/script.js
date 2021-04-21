821665a07b6b15601ed0346ed8e8f115ac78dd05
Vadim <57544626+Vadimpioner@users.noreply.github.com>
2021-04-22 02:04:31 +0300

    Add files via upload

--- VIKINGS/js/script.js
+++ VIKINGS/js/script.js
@@ -12,4 +12,34 @@ $(function() {
         arrows: true,
 
     });
+
+    const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    menuClose = document.querySelector('.menu__close'),
+    menuLink = document.querySelectorAll('.menu__season-list');
+
+    hamburger.addEventListener('click', () => {
+        menu.classList.add('active');
+        document.body.style.overflow = 'hidden'; // преостанавливаем скролл при появлении модального окна
+    });
+
+    menuClose.addEventListener('click', () => {
+        menu.classList.remove('active');
+        document.body.style.overflow = '';
+    });
+
+    // menuLink.forEach(item => {
+    //     item.querySelector("a").addEventListener('click', () => {
+    //         menu.classList.remove('active');
+    //         document.body.style.overflow = '';
+
+    //     });
+    // });
+
+document.addEventListener('keydown', (e) => { // событие keydown срабатывает при нажатии кнопок
+    if (e.code === 'Escape' && menu.classList.contains('active')) {
+        menu.classList.remove('active');
+        document.body.style.overflow = '';
+    }
+});
 });
